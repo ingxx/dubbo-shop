415c0a98a40c3d622e97e6b4e1d6be0338aedc11
e <q523969705@gmail.com>
2019-01-29 19:17:08 +0800

    Initial commit

---
+++ pyg-dao/src/main/java/top/ingxx/mapper/TbSpecificationMapper.java
@@ -0,0 +1,30 @@
+package top.ingxx.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.ingxx.pojo.TbSpecification;
+import top.ingxx.pojo.TbSpecificationExample;
+
+public interface TbSpecificationMapper {
+    int countByExample(TbSpecificationExample example);
+
+    int deleteByExample(TbSpecificationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbSpecification record);
+
+    int insertSelective(TbSpecification record);
+
+    List<TbSpecification> selectByExample(TbSpecificationExample example);
+
+    TbSpecification selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbSpecification record, @Param("example") TbSpecificationExample example);
+
+    int updateByExample(@Param("record") TbSpecification record, @Param("example") TbSpecificationExample example);
+
+    int updateByPrimaryKeySelective(TbSpecification record);
+
+    int updateByPrimaryKey(TbSpecification record);
+}
